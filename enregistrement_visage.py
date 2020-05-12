3e070aa21d5dde00dd17b52e4efd69280029bd20
Robin <robin.lemancel@etudiant.univ-rennes1.fr>
2020-05-12 09:57:02 +0200

    modification script enregistrement visage pour pouvoir etre appelé depuis php avec des paramètres

--- enregistrement_visage.py
+++ enregistrement_visage.py
@@ -1,35 +1,52 @@
 import cv2
-import operator
+import sys
+import os
 import common as c
 
-face_cascade=cv2.CascadeClassifier("./haarcascade_frontalface_alt2.xml")
-# Create a VideoCapture object and read from input file 
+# le fichier doit être appelé comme suit
+# python enregistrement_visage.py XXX.mp4 YYY
+# XXX étant le nom de la vidéo et YYY le nom du répertoire dans lequel on stocke les photos (robin par exemple)
 
-# 0 pour utiliser la caméra
-cap=cv2.VideoCapture(0)
+face_cascade = cv2.CascadeClassifier("./haarcascade_frontalface_alt2.xml")
+# cette utilisation par arguments passés ne permet plus l'utilisation avec une cam de l'ordinateur
+# il faut donner une vidéo en paramètre
+cap = cv2.VideoCapture(sys.argv[1])
+img_non_classees = 'photos/personnes_connues/' + sys.argv[2]
+
+# Variable paramétrable
+# définit le nombre d'images qu'on laisse passer entre deux enregistrements d'images quand on lit une vidéo
+entre_deux_images = 15
 
 
-id=0
+if not os.path.isdir(img_non_classees):
+    os.mkdir(img_non_classees)
+
+id = 0
 while True:
-    # lire la vidéo image par image
-    ret, frame=cap.read()
-    gray=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    face=face_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=4, minSize=(c.min_size, c.min_size))
-    # récupérer le visage via un quadruplet
-    for x, y, w, h in face:
-        cv2.imwrite("non-classees/p-{:d}.png".format(id), frame[y:y+h, x:x+w])
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
-        id+=1
-    key=cv2.waitKey(5)
-    # quitter
-    if key==ord('q'):
+    ret, frame = cap.read()
+    if ret is False:
         break
-    # lire sans image (avance rapide)
-    if key==ord('a'):
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # détecte des objets de différentes tailles dans l'image d'entrée.
+    # les objets détectés sont renvoyés sous forme de liste de rectangles.
+    face = face_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=4, minSize=(c.min_size, c.min_size))
+    for x, y, w, h in face:
+        cv2.imwrite("{}/p-{:d}.png".format(img_non_classees, id), frame[y:y + h, x:x + w])
+        # TODO: supprimer la création des rectangles qui ne sert que pour nos tests
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+        id += 1
+    # TODO: supprimer la gestion des keys qui ne sert que pour nos tests
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord('q'):
+        break
+    if key == ord('a'):
         for cpt in range(100):
-            ret, frame=cap.read()
+            ret, frame = cap.read()
+    # TODO: supprimer l'affichage de la fenetre de la vidéo qui ne sert que pour nos tests
     cv2.imshow('video', frame)
-    for cpt in range(4):
-        ret, frame=cap.read()
+    for cpt in range(entre_deux_images):
+        ret, frame = cap.read()
+
 cap.release()
+# TODO: supprimer la destruction de la fenetre qui ne sert que pour nos tests
 cv2.destroyAllWindows()
--- enregistrement_visage_old.py
+++ enregistrement_visage_old.py
@@ -1,37 +1,35 @@
 import cv2
 import operator
-import os
 import common as c
 
 face_cascade=cv2.CascadeClassifier("./haarcascade_frontalface_alt2.xml")
-#cap=cv2.VideoCapture("Hu.mp4")
-cap=cv2.VideoCapture(0)
-img_non_classees='non-classees'
+# Create a VideoCapture object and read from input file 
 
-if not os.path.isdir(img_non_classees):
-    os.mkdir(img_non_classees)
-    
+# 0 pour utiliser la caméra
+cap=cv2.VideoCapture(0)
+
+
 id=0
 while True:
+    # lire la vidéo image par image
     ret, frame=cap.read()
-    if ret is False:
-        break
     gray=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    #Détecte des objets de différentes tailles dans l'image d'entrée. Les objets détectés sont renvoyés sous forme de liste de rectangles.
     face=face_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=4, minSize=(c.min_size, c.min_size))
+    # récupérer le visage via un quadruplet
     for x, y, w, h in face:
-        cv2.imwrite("{}/p-{:d}.png".format(img_non_classees, id), frame[y:y+h, x:x+w])
+        cv2.imwrite("non-classees/p-{:d}.png".format(id), frame[y:y+h, x:x+w])
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
         id+=1
-    key=cv2.waitKey(1)&0xFF
+    key=cv2.waitKey(5)
+    # quitter
     if key==ord('q'):
         break
+    # lire sans image (avance rapide)
     if key==ord('a'):
         for cpt in range(100):
             ret, frame=cap.read()
     cv2.imshow('video', frame)
     for cpt in range(4):
         ret, frame=cap.read()
-
 cap.release()
 cv2.destroyAllWindows()

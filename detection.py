1d278a008c0228953e6cfb5fd66db44a2b5bacc9
Robin <robin.lemancel@etudiant.univ-rennes1.fr>
2020-05-12 11:40:11 +0200

    modif pour prendre photo à la place de vidéo pour detection animal

--- detection.py
+++ detection.py
@@ -6,6 +6,8 @@ import envoimail
 
 tf.disable_v2_behavior()
 
+face_cascade = cv2.CascadeClassifier("./haarcascade_frontalface_alt2.xml")
+
 labels = {
     1: "person",
     2: "bicycle",
@@ -89,19 +91,36 @@ labels = {
     90: "toothbrush"
 }
 
+chemin_animaux = "photos/enregistrement_journalier/animaux/"
+switcher = {
+    16: "oiseaux",
+    17: "chats",
+    18: "chiens",
+    19: "chevaux",
+    20: "moutons",
+    21: "vaches",
+    22: "elephants",
+    23: "ours",
+    24: "zebres",
+    25: "girafes"
+}
+
 MODEL_NAME = 'ssd_mobilenet_v2_coco_2018_03_29'
 PATH_TO_FROZEN_GRAPH = MODEL_NAME + '/frozen_inference_graph.pb'
 color_infos = (255, 255, 0)
 fichier_video = None
-nom_fichier = ""
+fichier_photo = None
+nom_video = ""
+nom_photo = ""
 cpt_fin_mouvement = -1
+
 
 # variables parametrables
 dir_videos = "c:\\enregistrements\\"  # le répertoire dans lequel sont enregistrés les vidéos
 precision_retenue = 0.50  # l'indice de confiance minimum pour détecter un objet
 fin_mouvement = 40  # le nombre d'images sans détection avant de couper la vidéo
 source_video = 0  # la source de la vidéo, 0 pour cam intégré, nom d'un fichier pour vidéo
-destinataire = ""  # l'adresse mail du destinataire à qui sera envoyé le mail
+destinataire = "robin.lemancel@gmail.com"  # l'adresse mail du destinataire à qui sera envoyé le mail
 
 
 # *************************** Partie IA tensorflow *************************************
@@ -119,7 +138,6 @@ with detection_graph.as_default():
 
 with detection_graph.as_default():
     with tf.Session() as sess:
-        # cap=cv2.VideoCapture('video1.mp4')
         cap = cv2.VideoCapture(source_video)  # récupére les images de la webcam intégré
         ops = tf.get_default_graph().get_operations()  # retourne la liste des opérations dans le graphe
         all_tensor_names = {output.name for op in ops for output in op.outputs}
@@ -151,6 +169,7 @@ with detection_graph.as_default():
 
                 if scores[objet] > precision_retenue:  # si le score est supérieur au paramétrage
                     if classes[objet] in {1, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25}:
+                        # TODO: supprimer les lignes suivantes qui ne nous servent que pour les tests
                         height, width = frame.shape[:2]
                         xmin = int(xmin * width)
                         xmax = int(xmax * width)
@@ -161,29 +180,55 @@ with detection_graph.as_default():
                         cv2.putText(frame, txt, (xmin, ymin - 5), cv2.FONT_HERSHEY_PLAIN, 1, color_infos, 2)
 
                     if classes[objet] in {16, 17, 18, 19, 20, 21, 22, 23, 24, 25}:  # si un animal est détecté
-                        # on crée un fichier video s'il s'agit de la premiere image enregistrée
-                        if fichier_video is None:
-                            nom_fichier = time.strftime("%Y_%m_%d_%H_%M_%S") + ".avi"
-                            fichier_video = dir_videos + nom_fichier
-                            video = cv2.VideoWriter(fichier_video, cv2.VideoWriter_fourcc(*'DIVX'), 15,
-                                                    (largeur, hauteur))
+
+                        # TODO???: ajouter un controle pour tester si le chat apparait sur plusieurs frames
+                        #  avant d'enregistrer l'image pour éviter les faux positifs
+
+                        # recupere le répertoire ou enregistrer la photo de l'animal détecté
+                        dir_photos = chemin_animaux + switcher.get(classes[objet]) + "/"
+
+                        # on crée un fichier photo
+                        if fichier_photo is None:
+                            nom_photo = time.strftime("%Y_%m_%d_%H_%M_%S") + ".png"
+                            fichier_photo = dir_photos + nom_photo
+                            cv2.imwrite(fichier_photo, frame)
 
                         cpt_fin_mouvement = fin_mouvement  # on initialise ou réinitialise le compteur
 
-            if cpt_fin_mouvement > 0:
-                video.write(frame)  # on écrit dans la video tant que le compteur n'atteint pas 0
+                        # on crée un fichier video s'il s'agit de la premiere image enregistrée
+                        # if fichier_video is None:
+                        #    nom_fichier = time.strftime("%Y_%m_%d_%H_%M_%S") + ".avi"
+                        #    fichier_video = dir_videos + nom_fichier
+                        #    video = cv2.VideoWriter(fichier_video, cv2.VideoWriter_fourcc(*'DIVX'), 15,
+                        #                            (largeur, hauteur))
 
+                        # cpt_fin_mouvement = fin_mouvement  # on initialise ou réinitialise le compteur
+
+            # if cpt_fin_mouvement > 0:
+            #     video.write(frame)  # on écrit dans la video tant que le compteur n'atteint pas 0
+
+            # si l'animal n'a pas été détecté depuis cpt_fin_mouvement frame, on envoie un mail et on réinitialise
             if cpt_fin_mouvement == 0:
-                envoimail.envoyermail(destinataire, "Intrusion",
-                                      "Un pangolin sauvage a été détecté", dir_videos, nom_fichier)
-                video.release()  # on cloture la video si aucun animal n'a été détecté depuis cpt_fin_mouvement images
-                fichier_video = None
-                nom_fichier = ""
+                envoimail.envoyermail(destinataire, "Détection animal", "Un animal sauvage a été détecté",
+                                      dir_photos, nom_photo)
+                fichier_photo = None
+                nom_photo = ""
 
             cpt_fin_mouvement = cpt_fin_mouvement - 1
 
+            # if cpt_fin_mouvement == 0:
+            #     envoimail.envoyermail(destinataire, "Intrusion",
+            #                           "Un pangolin sauvage a été détecté", dir_videos, nom_fichier)
+            #     video.release()  # on cloture la video si aucun animal n'a été détecté depuis cpt_fin_mouvement images
+            #     fichier_video = None
+            #     nom_fichier = ""
+
+            # cpt_fin_mouvement = cpt_fin_mouvement - 1
+
+            # TODO: supprimer l'affichage de la fenetre qui ne nous sert que pour les tests
             cv2.imshow('image', frame)
 
+            # TODO: supprimer la gestion des keys qui ne nous sert que pour les tests
             key = cv2.waitKey(1) & 0xFF
             if key == ord('a'):
                 for objet in range(500):
@@ -192,4 +237,5 @@ with detection_graph.as_default():
                 break
 
         cap.release()
+        # TODO: supprimer la destruction de fenetre qui ne nous sert que pour les tests
         cv2.destroyAllWindows()

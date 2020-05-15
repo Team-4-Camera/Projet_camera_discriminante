8c446afd490c57610a5041583e88f1907f7ab6b9
Robin <robin.lemancel@etudiant.univ-rennes1.fr>
2020-05-15 11:10:11 +0200

    bonne structure de repertoires

---
+++ creation_repertoire.py
@@ -0,0 +1,23 @@
+import os
+
+tableau_chemins = [
+    "photos",
+    "photos/personnes_connues",
+    "photos/enregistrement_journalier",
+    "photos/enregistrement_journalier/humains",
+    "photos/enregistrement_journalier/animaux",
+    "photos/enregistrement_journalier/animaux/oiseaux",
+    "photos/enregistrement_journalier/animaux/chats",
+    "photos/enregistrement_journalier/animaux/chiens",
+    "photos/enregistrement_journalier/animaux/chevaux",
+    "photos/enregistrement_journalier/animaux/moutons",
+    "photos/enregistrement_journalier/animaux/vaches",
+    "photos/enregistrement_journalier/animaux/elephants",
+    "photos/enregistrement_journalier/animaux/ours",
+    "photos/enregistrement_journalier/animaux/zebres",
+    "photos/enregistrement_journalier/animaux/girafes"
+]
+
+for chemin in tableau_chemins:
+    if not os.path.isdir(chemin):
+        os.mkdir(chemin)
